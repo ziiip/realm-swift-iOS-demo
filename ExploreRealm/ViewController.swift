82a58c839160a082dc13346f86acc42ef303c6f2
S. M. Sajid Hasan Shanta <sajidhasan673@gmail.com>
2024-02-08 17:47:29 +0600

    Define a Realm Object Model and insert data

--- ExploreRealm/ViewController.swift
+++ ExploreRealm/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
+    
+    let realm = try! Realm()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createUser(firstName: "Sajid", lastName: "Hasan", age: 23)
     }
-
-
+    
+    fileprivate func createUser(firstName: String, lastName: String, age: Int) {
+        let newUser = User()
+        newUser.firstName = firstName
+        newUser.lastName = lastName
+        newUser.age = age
+        
+        realm.beginWrite()
+        realm.add(newUser)
+        try! realm.commitWrite()
+    }
 }
 
+class User: Object {
+    @objc dynamic var id: String = UUID().uuidString
+    @objc dynamic var firstName: String = ""
+    @objc dynamic var lastName: String = ""
+    @objc dynamic var age: Int = 0
+}
